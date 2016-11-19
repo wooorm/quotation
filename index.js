f3c8cd606184d1170431f39cc8e73a6fa765236a
Titus Wormer <tituswormer@gmail.com>
2016-11-19 17:32:40 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,78 +1,33 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module quotation
- * @fileoverview Quote a value.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/*
- * Constants.
- */
+module.exports = quotation;
 
 var C_DEFAULT = '"';
 
-/**
- * Quote text.
- *
- * @example
- *   quotation('one');
- *   // '"one"'
- *
- * @example
- *   quotation(['one', 'two']);
- *   // ['"one"', '"two"']
- *
- * @example
- *   quotation('one', '\'');
- *   // '\'one\''
- *
- * @example
- *   quotation('one', '“', '”');
- *   // '“one”'
- *
- * @param {string|Array.<string>} value - One or more
- *   values to quote.
- * @param {string} [open='"'] - Opening quote.
- * @param {string} [close] - Closing quote, defaults to
- *   `open` or the default (`"`) when not given.
- * @return {string} - Quoted `value`.
- * @throws {Error} - When not given `string` or
- *   `Array.<string>`.
- */
+/* Quote text. */
 function quotation(value, open, close) {
-    var result;
-    var index;
-    var length;
+  var result;
+  var index;
+  var length;
 
-    open = open || C_DEFAULT;
-    close = close || open;
+  open = open || C_DEFAULT;
+  close = close || open;
 
-    if (typeof value === 'string') {
-        return open + value + close;
-    }
+  if (typeof value === 'string') {
+    return open + value + close;
+  }
 
-    if (typeof value !== 'object' || !('length' in value)) {
-        throw new Error('Expected string or array of strings');
-    }
+  if (typeof value !== 'object' || !('length' in value)) {
+    throw new Error('Expected string or array of strings');
+  }
 
-    result = [];
-    length = value.length;
-    index = -1;
+  result = [];
+  length = value.length;
+  index = -1;
 
-    while (++index < length) {
-        result[index] = quotation(value[index], open, close);
-    }
+  while (++index < length) {
+    result[index] = quotation(value[index], open, close);
+  }
 
-    return result;
+  return result;
 }
-
-/*
- * Expose.
- */
-
-module.exports = quotation;
--- test.js
+++ test.js
@@ -1,78 +1,60 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module quotation
- * @fileoverview Test suite for `quotation`.
- */
-
 'use strict';
-
-/* eslint-env node */
-
-/*
- * Dependencies.
- */
 
 var test = require('tape');
 var quotation = require('./');
 
-/*
- * Tests.
- */
-
 test('quotation(url)', function (t) {
-    t.throws(
-        function () {
-            quotation(true);
-        },
-        /Error: Expected string or array of strings/,
-        'should fail when given a boolean'
-    );
+  t.throws(
+    function () {
+      quotation(true);
+    },
+    /Error: Expected string or array of strings/,
+    'should fail when given a boolean'
+  );
 
-    t.throws(
-        function () {
-            quotation({});
-        },
-        /Error: Expected string or array of strings/,
-        'should fail when given an object'
-    );
+  t.throws(
+    function () {
+      quotation({});
+    },
+    /Error: Expected string or array of strings/,
+    'should fail when given an object'
+  );
 
-    t.equal(
-        quotation('a'),
-        '"a"',
-        'should quote a string'
-    );
+  t.equal(
+    quotation('a'),
+    '"a"',
+    'should quote a string'
+  );
 
-    t.deepEqual(
-        quotation(['a', 'b']),
-        ['"a"', '"b"'],
-        'should quote an array'
-    );
+  t.deepEqual(
+    quotation(['a', 'b']),
+    ['"a"', '"b"'],
+    'should quote an array'
+  );
 
-    t.equal(
-        quotation('a', '\''),
-        '\'a\'',
-        'should quote a string given `character`'
-    );
+  t.equal(
+    quotation('a', '\''),
+    '\'a\'',
+    'should quote a string given `character`'
+  );
 
-    t.deepEqual(
-        quotation(['a', 'b'], '\''),
-        ['\'a\'', '\'b\''],
-        'should quote an array given `character`'
-    );
+  t.deepEqual(
+    quotation(['a', 'b'], '\''),
+    ['\'a\'', '\'b\''],
+    'should quote an array given `character`'
+  );
 
-    t.equal(
-        quotation('a', '“', '”'),
-        '“a”',
-        'should quote a string given `open` and `close`'
-    );
+  t.equal(
+    quotation('a', '“', '”'),
+    '“a”',
+    'should quote a string given `open` and `close`'
+  );
 
-    t.deepEqual(
-        quotation(['a', 'b'], '“', '”'),
-        ['“a”', '“b”'],
-        'should quote an array given `open` and `close`'
-    );
+  t.deepEqual(
+    quotation(['a', 'b'], '“', '”'),
+    ['“a”', '“b”'],
+    'should quote an array given `open` and `close`'
+  );
 
-    t.end();
+  t.end();
 });
