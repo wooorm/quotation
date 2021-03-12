8c5e16f189e72762c4b3cc11c1ae6bf0271ff21e
Christian Murphy <christian.murphy.42@gmail.com>
2021-03-12 09:16:46 -0700

    Fix types
    Closes GH-6.

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 /**
- * Quote a value.
- *
- * @param {string | string[]} value Value(s) to wrap in quotes
- * @param {string} [open='"'] Opening quote
- * @param {string} [close=open] Closing quote
- * @returns {string | string[]}
+ * @typedef {{
+ *   (value: string, open?: string, close?: string): string
+ *   (value: string[], open?: string, close?: string): string[]
+ * }} Quotation
  */
-export function quotation(value, open, close) {
-  var start = open || '"'
-  var end = close || start
-  /** @type {string[]} */
-  var result = []
-  var index = -1
 
-  if (Array.isArray(value)) {
-    while (++index < value.length) {
-      result[index] = start + value[index] + end
+// prettier-ignore
+/** @type {Quotation} */
+export const quotation = (
+  /**
+   * Quote a value.
+   *
+   * @param {string | string[]} value Value(s) to wrap in quotes
+   * @param {string} [open='"'] Opening quote
+   * @param {string} [close=open] Closing quote
+   * @returns {string | string[]}
+   */
+  function (value, open, close) {
+    var start = open || '"'
+    var end = close || start
+    /** @type {string[]} */
+    var result = []
+    var index = -1
+
+    if (Array.isArray(value)) {
+      while (++index < value.length) {
+        result[index] = start + value[index] + end
+      }
+
+      return result
     }
 
-    return result
-  }
+    if (typeof value === 'string') {
+      return start + value + end
+    }
 
-  if (typeof value === 'string') {
-    return start + value + end
+    throw new TypeError('Expected string or array of strings')
   }
-
-  throw new TypeError('Expected string or array of strings')
-}
+)
