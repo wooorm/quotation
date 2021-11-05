e1b3b1a9ef459e0f926e884dfce48de4351c4a03
Titus Wormer <tituswormer@gmail.com>
2021-11-05 20:24:33 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-// prettier-ignore
-/** @type {{
- *   (value: string, open?: string, close?: string): string
- *   (value: string[], open?: string, close?: string): string[]
- * }} */
-export var quotation = (
+/**
+ * Quote a value.
+ *
+ * @param value
+ *   Value(s) to wrap in quotes
+ * @param [open='"']
+ *   Opening quote
+ * @param [close=open]
+ *   Closing quote
+ */
+export const quotation =
   /**
-   * Quote a value.
-   *
-   * @param {string | string[]} value Value(s) to wrap in quotes
-   * @param {string} [open='"'] Opening quote
-   * @param {string} [close=open] Closing quote
-   * @returns {string | string[]}
+   * @type {{
+   *   (value: string, open?: string, close?: string): string
+   *   (value: string[], open?: string, close?: string): string[]
+   * }}
    */
-  function (value, open, close) {
-    var start = open || '"'
-    var end = close || start
-    /** @type {string[]} */
-    var result = []
-    var index = -1
+  (
+    /**
+     * @param {string|Array<string>} value
+     * @param {string} open
+     * @param {string} close
+     * @returns {string|string[]}
+     */
+    function (value, open, close) {
+      const start = open || '"'
+      const end = close || start
+      /** @type {string[]} */
+      const result = []
+      let index = -1
 
-    if (Array.isArray(value)) {
-      while (++index < value.length) {
-        result[index] = start + value[index] + end
+      if (Array.isArray(value)) {
+        while (++index < value.length) {
+          result[index] = start + value[index] + end
+        }
+
+        return result
       }
 
-      return result
-    }
+      if (typeof value === 'string') {
+        return start + value + end
+      }
 
-    if (typeof value === 'string') {
-      return start + value + end
+      throw new TypeError('Expected string or array of strings')
     }
-
-    throw new TypeError('Expected string or array of strings')
-  }
-)
+  )
